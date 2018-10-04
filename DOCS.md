1a18ffde448fd53d30c1b12d80e1a97161ef3e05
Reda Adiyasa <reda@savantdegrees.com>
2018-10-04 19:19:31 +0800

    remove browser-sync

--- DOCS.md
+++ DOCS.md
@@ -7,5 +7,5 @@ You'll need to install Node and Gulp. Here's a walkthrough of how to get a proje
 2. [Install Gulp](http://Gulpjs.com/) using `npm install -g gulp`. You may need to use `sudo` in front of the Gulp install command to give it permissions.
 3. Fork/Clone/Download the artreibp repository into your machine, you should hopefully see all the files and folders.
 4. Open Terminal and install dependencies to `node_modules` directory in your project directory using `npm install`. You don't need `sudo` to do this.
-5. The `npm install` you did in previous step should install all the dependencies, which you can confirm by visiting the `node_modules` in your project directory. Then use `gulp` (again in your project directory) to run the commands and to automatically open the page running on `localhost:3002`.
+5. The `npm install` you did in previous step should install all the dependencies, which you can confirm by visiting the `node_modules` in your project directory. Then use `gulp` (again in your project directory) to run the commands.
 6. From now on, just run `gulp` in your project directory to automatically run Gulp tasks.
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
     autoprefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
     jshint = require('gulp-jshint'),
@@ -9,8 +8,9 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     cssnano = require('gulp-cssnano'),
     sourcemaps = require('gulp-sourcemaps'),
-    package = require('./package.json');
-
+    package = require('./package.json'),
+    cssDir = 'dist/assets/css',
+    jsDir = 'dist/assets/js';
 
 var banner = [
   '/*!\n' +
@@ -29,13 +29,12 @@ gulp.task('css', function () {
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer('last 4 version'))
-    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(gulp.dest(cssDir))
     .pipe(cssnano())
     .pipe(rename({ suffix: '.min' }))
     .pipe(header(banner, { package : package }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/assets/css'))
-    .pipe(browserSync.reload({stream:true}));
+    .pipe(gulp.dest(cssDir))
 });
 
 gulp.task('js',function(){
@@ -44,29 +43,16 @@ gulp.task('js',function(){
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
     .pipe(header(banner, { package : package }))
-    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(gulp.dest(jsDir))
     .pipe(uglify())
     .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
     .pipe(header(banner, { package : package }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/assets/js'))
-    .pipe(browserSync.reload({stream:true, once: true}));
+    .pipe(gulp.dest(jsDir))
 });
 
-gulp.task('browser-sync', function() {
-    browserSync.init(null, {
-        server: {
-            baseDir: "dist"
-        }
-    });
-});
-gulp.task('bs-reload', function () {
-    browserSync.reload();
-});
-
-gulp.task('default', ['css', 'js', 'browser-sync'], function () {
+gulp.task('default', ['css', 'js'], function () {
     gulp.watch("src/scss/**/*.scss", ['css']);
     gulp.watch("src/js/*.js", ['js']);
-    gulp.watch("*.html", ['bs-reload']);
 });
